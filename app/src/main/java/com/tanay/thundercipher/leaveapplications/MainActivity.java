7d6335994a586a7348e5ee0492cce9b3c165aa9e
Tanay Saxena <sstanay@gmail.com>
2020-07-24 22:32:37 +0530

    Added title and inserted website navigation

--- app/src/main/java/com/tanay/thundercipher/leaveapplications/MainActivity.java
+++ app/src/main/java/com/tanay/thundercipher/leaveapplications/MainActivity.java
@@ -2,9 +2,32 @@ package com.tanay.thundercipher.leaveapplications;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView websiteImageView;
+
+    private void clickedButton(String url)
+    {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+
+        try
+        {
+            startActivity(Intent.createChooser(intent, "Open With:"));
+        }
+
+        catch(Exception e)
+        {
+            Toast.makeText(getApplicationContext(), "Couldn't open the link!", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +35,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setTitle("Application Authorizer");
+
+        websiteImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                clickedButton("https://www.iiitm.ac.in/index.php/en/");
+            }
+        });
     }
 }
