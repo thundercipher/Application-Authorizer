5656f0bd2e0b3a8a91c67e5b89d1c7e95290746b
Tanay Saxena <sstanay@gmail.com>
2020-08-02 01:06:47 +0530

    Created LogIn and SignUp activities

--- app/src/main/java/com/tanay/thundercipher/leaveapplications/LogInActivity.java
+++ app/src/main/java/com/tanay/thundercipher/leaveapplications/LogInActivity.java
@@ -10,5 +10,7 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+
     }
 }
