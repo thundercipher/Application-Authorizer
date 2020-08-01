ee0f506e1f22db3fc35b15cd38141e3de3392260
Tanay Saxena <sstanay@gmail.com>
2020-08-02 01:10:18 +0530

    Revert "Created LogIn and SignUp activities"
    This reverts commit c821079a

--- app/src/main/java/com/tanay/thundercipher/leaveapplications/MainActivity.java
+++ app/src/main/java/com/tanay/thundercipher/leaveapplications/MainActivity.java
@@ -5,16 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView websiteImageView, linkedinImageView;
-    Button logInButton, signUpButton;
 
     private void clickedButton(String url)
     {
@@ -38,10 +35,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setTitle("Application Authorizer");
-        websiteImageView = (ImageView)findViewById(R.id.websiteImageView);
-        linkedinImageView = (ImageView)findViewById(R.id.linkedinImageView);
-        logInButton = (Button)findViewById(R.id.logInButton);
-        signUpButton = (Button)findViewById(R.id.signUpButton);
 
         websiteImageView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,23 +49,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v)
             {
                 clickedButton("//https://www.linkedin.com/school/abv-indian-institute-of-information-technology-and-management/");
-            }
-        });
-
-        logInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v)
-            {
-                Intent i = new Intent(getApplicationContext(), LogInActivity.class);
-                startActivity(i);
-            }
-        });
-
-        signUpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v)
-            {
-                Intent i = new Intent(getApplicationContext(), SignUpActivity.class);
             }
         });
     }
