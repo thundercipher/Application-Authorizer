688de47d2f854a079ca1c673001358babad6a3b4
Tanay Saxena <sstanay@gmail.com>
2020-08-02 12:56:29 +0530

    Added spinner in LogInActivity

--- app/src/main/java/com/tanay/thundercipher/leaveapplications/LogInActivity.java
+++ app/src/main/java/com/tanay/thundercipher/leaveapplications/LogInActivity.java
@@ -3,12 +3,25 @@ package com.tanay.thundercipher.leaveapplications;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class LogInActivity extends AppCompatActivity {
+
+    Spinner usersListSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        usersListSpinner = (Spinner)findViewById(R.id.usersListSpinner);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.users, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        usersListSpinner.setAdapter(adapter);
     }
 }
