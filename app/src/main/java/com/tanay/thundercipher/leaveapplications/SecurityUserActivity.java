85012be23947f22ac70873d5c39991c4454cb8f6
Tanay Saxena <sstanay@gmail.com>
2020-08-22 17:57:29 +0530

    Added toolbars to the user activities

--- app/src/main/java/com/tanay/thundercipher/leaveapplications/SecurityUserActivity.java
+++ app/src/main/java/com/tanay/thundercipher/leaveapplications/SecurityUserActivity.java
@@ -1,15 +1,48 @@
 package com.tanay.thundercipher.leaveapplications;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class SecurityUserActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId())
+        {
+            case R.id.menuActionLogout :
+            {
+                /*
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(UserActivity.this, "Successfully logged out!", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(UserActivity.this, MainActivity.class));
+                */
+                return true;
+            }
+
+            default:
+            {
+                return super.onOptionsItemSelected(item);
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +65,5 @@ public class SecurityUserActivity extends AppCompatActivity {
         });
 
         */
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.toolbar_menu, menu);
-
-        return true;
     }
 }
